8a80ab0e72a130a682dea01ac617fcf854efb1c9
M.SAFYAN NASIR <safyannasir68@gmail.com>
2026-04-07 02:23:44 +0500

    changing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("../public/heroimage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+      <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/heroimage.png")] bg-no-repeat bg-cover bg-center h-screen'>
         <p className="bg-[#49B9FF]/50 px-3 py-1 rounded-full mt-20 inline-block">
           The Ultimate Hotel Experience
         </p>
