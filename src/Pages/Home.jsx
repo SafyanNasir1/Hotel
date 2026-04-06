7e7589dcc57e380970c2003e5b050e9bbff341f1
M.SAFYAN NASIR <safyannasir68@gmail.com>
2026-04-07 02:30:46 +0500

    changing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
       <Navbar />
       {/* <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/heroimage.png")] bg-no-repeat bg-cover bg-center h-screen'> */}
       <div
-        style={{ backgroundImage: "url('/heroimage.png')" }}
+        style={{ backgroundImage: "url('/heroimage.png')" }} // Fixed: Removed ./public
         className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-no-repeat bg-cover bg-center h-screen"
       >
         <p className="bg-[#49B9FF]/50 px-3 py-1 rounded-full mt-20 inline-block">
@@ -39,15 +39,15 @@ const Home = () => {
         <form className="bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto">
           <div>
             <div className="flex items-center gap-2">
-              {/* <img src={assets.calenderIcon} alt="" className="h-4" /> */}
-              <img src="./public/calenderIcon.svg" alt="" className="h-4" />
+              {/* Fixed: Removed ./public from src */}
+              <img src="/calenderIcon.svg" alt="calendar" className="h-4" />
               <label htmlFor="destinationInput">Destination</label>
             </div>
             <input
               list="destinations"
               id="destinationInput"
               type="text"
-              className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
+              className="rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
               placeholder="Type here"
               required
             />
@@ -62,35 +62,25 @@ const Home = () => {
 
           <div>
             <div className="flex items-center gap-2">
-              {/* <img src={assets.calenderIcon} alt="" className="h-4" /> */}
-              <img
-                src="./public/calenderIcon.svg"
-                alt="calender-icon"
-                className="h-4"
-              />
+              <img src="/calenderIcon.svg" alt="calendar" className="h-4" />
               <label htmlFor="checkIn">Check in</label>
             </div>
             <input
               id="checkIn"
               type="date"
-              className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
+              className="rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
             />
           </div>
 
           <div>
             <div className="flex items-center gap-2">
-              {/* <img src={assets.calenderIcon} alt="" className="h-4" /> */}
-              <img
-                src="./public/calenderIcon.svg"
-                alt="calender-icon"
-                className="h-4"
-              />
+              <img src="/calenderIcon.svg" alt="calendar" className="h-4" />
               <label htmlFor="checkOut">Check out</label>
             </div>
             <input
               id="checkOut"
               type="date"
-              className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
+              className="rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none"
             />
           </div>
 
@@ -101,7 +91,7 @@ const Home = () => {
               max={4}
               id="guests"
               type="number"
-              className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16"
+              className="rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none max-w-16"
               placeholder="0"
             />
           </div>
@@ -110,11 +100,8 @@ const Home = () => {
             onClick={handleSearch}
             className="flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1"
           >
-            <img
-              src="./public/searchIcon.svg"
-              alt="search-icon"
-              className="h-6"
-            />
+            {/* Fixed: Removed ./public from src */}
+            <img src="/searchIcon.svg" alt="search-icon" className="h-6" />
             <span>Search</span>
           </button>
         </form>
